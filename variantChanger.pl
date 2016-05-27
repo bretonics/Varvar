3b5edb285792c38ef5a34b8d6d6219e59b81a9ae
Andres Breton <bretonics@users.noreply.github.com>
2016-05-27 13:33:38 -0400

    stupid MS Windows changing file formats

--- variantChanger.pl
+++ variantChanger.pl
@@ -22,12 +22,12 @@ use MyIO;
 my $SEQFILE;
 my $SNPFILE;
 my $OUTFILE;
-my $usage = "\n\n $0 [options]\n
+my $usage = "\n\n$0 [options]\n
 Options:
-    -seq    Sequence file to change filtered SNPs
-    -snp    SNP file from snpExtract containing filtered SNPs to change
-    -out    Out file name
-    -help   Shows this message
+    -seq        Sequence file to change filtered SNPs
+    -snp        SNP file from snpExtract containing filtered SNPs to change
+    -out        Out file name
+    -help       Shows this message
 
 ";
 
@@ -61,74 +61,65 @@ changeSNPs($SEQFILE, $SNPFILE, $OUTFILE);
 #- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 sub checkCLA {
     unless ($SEQFILE && $SNPFILE && $OUTFILE) {
-        die "You did not provide all file names.", $!;
+        die "You did not provide all file names.", $!, $usage;
     }
     return;
 }
 
 sub changeSNPs {
     my ($seqFile, $snpFile, $outFile) = @_;
-    my $lineLen = _getLineLen($seqFile); #get FASTA sequence char/line count
-    my $snpFH = getFH("<", $snpFile);
-    my $outFH = getFH(">", $outFile);
-    while (<$snpFH>) {
+    my $SNPFH = getFH("<", $snpFile);
+    my $OUTFH = getFH(">", $outFile);
+    while (<$SNPFH>) {
         next if $. == 1; #skip header
-        my $position = $_ =~ /^?\t.+\t.+\t(\d+)\t.*/;
-        my $type = _variantLogic($_); #handle variant logic
-
+        my ($refPosition, $type, $variant, $variantLen) = _variantLogic($_); #handle variant logic, get type & length
+say $refPosition, $type, $variant, $variantLen; exit;
         # Open Sequence File
-        my $seqFH = getFH("<", $seqFile);
+        my $SEQFH = getFH("<", $seqFile);
         my $startPos = 1;
-        my $endPos = $lineLen;
-        while (<$seqFH>) {
+        my $endPos = 0;
+        while (<$SEQFH>) {
+            print $OUTFH $_; #print line to new seq file
             next if $. == 1;
-            $startPos = $startPos + $lineLen unless $. == 2; #seq line start position count
-            $endPos = $endPos + $lineLen; #seq line end position count
-            if ( $startPos < $position > $endPos) {
+            my $lineLen = length($_); #get FASTA sequence char/line count
+            $startPos = $startPos + $lineLen unless $. == 2; #seq line start position, ommit 1st line redundancy
+            $endPos = $endPos + $lineLen; #seq line end position
+            if ( $refPosition >= $startPos and $refPosition <= $endPos) {
                 my @seq = split('', $_); #split seq line into array
-                # Handle SNP Type
-                if ($type eq "insert") {
-                    splice(@seq, $position, $varLen, $snp); #insert in sequence
-                } elsif ($type eq "delete") {
-                    splice(@seq, $position, $varLen, $snp); #delete in sequence
-                } elsif ($type eq "snp") {
-                    splice(@seq, $position, $varLen, $snp); #replace snp in sequence
+                my $offset = $refPosition - $startPos; #handle position number within current substring line
+                # Handle SNP Type -> modify
+                if ($type eq "insertion") {
+                    splice(@seq, $offset, $variantLen,  $variant,); #insert in sequence
+                } elsif ($type eq "deletion") {
+                    splice(@seq, $offset, $variantLen); #delete in sequence
+                } elsif ($type eq "SNP") {
+                    $seq[$offset] =  $variant; #replace snp in sequence
                 } else {
-                    #something is really wrong
+                    die "Something is really wrong. Could not modify variant ($variant).\n", $!;
                 }
-                return @seq; #return modified sequence line
+                print $OUTFH @seq; #print modified sequence line
             }
-
-        }
-
-    }
-
-
-    _getLineNum();
+        } close $SEQFH; close $OUTFH;
+    } close $SNPFH;
 }
 
-sub _getLineLen {
-    my ($file) = @_
-    while (<getFH("<", $file)>) {
-        next if $. == 1;
-        my $lineLen = length($_);
-        return $lineLen;
-    }
-}
 
 sub _variantLogic {
     my ($line) = @_;
-    my $type = $line =~ /^?\t.+\t.+\t\d+\t(.+)\t.*/; #get variant type INDEL/SNP
-    my $varLen = 1;
-    if ($type eq "Indel") {
-        # Handle insertion vs. deletion logic
-        $varLen = $line =~ /^?\t.+\t.+\t\d+\t.+\t.+\t(.+)\t.*/;
-        $varLen = length($varLen); #length of insertion
+    my ($refPosition, $type, $variant) = $line =~ /^\?\t.+\t\d+\t(\d+)\t(\w+)\t(\w+)\t\w+\t.*/; #get variant type Indel/SNP
+    my $variantLen = 0;
+    if ($type eq "Indel") { # Handle insertion vs. deletion logic, otherwise == SNP
+        if ($variant eq "-") { #probably an insertion in reference
+            $type = "insertion";
+            # Length of deletion may be > 1
+                        say "Variant type ($variant) taken as a deletion in reference";
+        } elsif ($variant =~ /\w+/) { #probably a deletion in reference
+            $type = "deletion";
+            $variantLen = length($variant); #take reference string length
+            say "Variant type ($variant) taken as an insertion in reference";
+        } else {
+            warn "Could not determine variant type ($variant), continuing as SNP...";
+        }
     }
-    return ($type, $varLen);
-}
-
-sub _getLineNum {
-    my () = @_;
-    return;
+    return ($refPosition, $type, $variant, $variantLen);
 }
